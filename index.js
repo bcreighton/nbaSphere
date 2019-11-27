a9bf792be25a59cc6e502ad08e1bd70a42a92315
Bryson Creighton <brysoncreighton@gmail.com>
2019-11-27 14:33:37 -0700

    Add player news from News API
    Pulling news from the News API functions properly within the given
    parameters.
    
    Error handeling has been added to address blank queries when pulling
    from the API.

--- index.js
+++ index.js
@@ -1,35 +1,67 @@
-function getData() {
+function getNBAPlayer() {
     
-    const fetchData = async () => {
+    const fetchNBAPlayer = async () => {
         try {
-            const dataRes = await fetch("https://api-nba-v1.p.rapidapi.com/players/lastName/johnso", {
+            const nbaPlayerRes = await fetch("https://api-nba-v1.p.rapidapi.com/players/lastName/harden", {
                 "method": "GET",
                 "headers": {
                     "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
                     "x-rapidapi-key": "cfc420df64msha7a3397e1f4de3ep182cedjsnca0bd8135c93"
                 }
             })
-            const data = await dataRes.json();
+            const nbaPlayerData = await nbaPlayerRes.json();
 
-            if(data.api.players.length === 0){
+            if(nbaPlayerData.api.players.length === 0){
+                // provide input value once developed
                 throw new Error('There are no players in the NBA with the lastname "Hard"; please try again');
             } else {
-                console.log(data);
+                console.log(nbaPlayerData);
             }
         } catch(e) {
             console.log(e);
         }
     }
 
-    fetchData();
+    fetchNBAPlayer();
 }
 
-function watchForm() {
+function getNBAPlayerNews() {
+    const fetchNBAPlayerNews = async () => {
+        try {
+            const nbaPlayerNewsRes = await fetch("https://newsapi.org/v2/everything?q=james-harden", {
+                "method": "GET",
+                "headers": {
+                    "X-Api-Key": "bbbae998198647ef8f363a9b624282de"
+                }
+            })
+            const nbaPlayerNews = await nbaPlayerNewsRes.json();
+
+            if(nbaPlayerNews.status === 'error'){
+                throw new Error(nbaPlayerNews.message);
+            }
+            
+            console.log(nbaPlayerNews);
+
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
+    fetchNBAPlayerNews();
+}
+
+function watchForms() {
     $('#nbaPlayerSearch').submit(event => {
         event.preventDefault();
 
-        getData();
+        getNBAPlayer();
+    })
+
+    $('#nbaPlayerNewsSearch').submit(event => {
+        event.preventDefault();
+
+        getNBAPlayerNews();
     })
 }
 
-$(watchForm);
+$(watchForms);
