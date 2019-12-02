e5e5e0ef738df9ac612662097a996a9530a98bd8
Bryson Creighton <brysoncreighton@gmail.com>
2019-12-01 20:11:06 -0700

    Continue dev remote

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-const youtubeAPIKey = 'AIzaSyBOeJM_9wrehRc1wH9v-gJE-RFsmjbuwps'
+'use strict'
 
-function getNBAPlayer() {
+const youtubeAPIKey = 'AIzaSyBOeJM_9wrehRc1wH9v-gJE-RFsmjbuwps';
+
+function displayNBAPlayerSearchResults(players) {
+    $('#searchResults').empty();
+
+    for(let i = 0; i < players.length; i++){
+        debugger;
+        let pos = players[i].leagues.standard.pos;
+        let jersey = players[i].leagues.standard.jersey;
+
+        if(pos === undefined){
+            pos = 'FA';
+        } 
+
+        if(jersey === undefined) {
+            jersey = 'N/A';
+        }
+
+
+        $('#searchResults').append(
+            `<li class='resultItem'>
+                <h3 class='playerName'>${players[i].firstName} ${players[i].lastName}</h3>
+                <p class='position'>${players[i].leagues.standard.pos}</p>
+                <p class='jerseyNumber'>${players[i].leagues.standard.jersey}</p>
+            </li>`
+        );
+    }
+
+    $('#searchResultsContainer').removeClass('hidden');
+}
+
+function getNBAPlayer(player) {
     
     const fetchNBAPlayer = async () => {
         try {
-            const nbaPlayerRes = await fetch("https://api-nba-v1.p.rapidapi.com/players/lastName/harden", {
+            const nbaPlayerRes = await fetch(`https://api-nba-v1.p.rapidapi.com/players/lastName/${player}`, {
                 "method": "GET",
                 "headers": {
                     "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
@@ -12,12 +43,14 @@ function getNBAPlayer() {
                 }
             })
             const nbaPlayerData = await nbaPlayerRes.json();
+            const nbaPlayers = nbaPlayerData.api.players;
 
-            if(nbaPlayerData.api.players.length === 0){
+            if(nbaPlayers.length === 0){
                 // provide input value once developed
                 throw new Error('There are no players in the NBA with the lastname "Hard"; please try again');
             } else {
-                console.log(nbaPlayerData);
+                displayNBAPlayerSearchResults(nbaPlayers)
+                console.log(nbaPlayers);
             }
         } catch(e) {
             console.log(e);
@@ -27,101 +60,264 @@ function getNBAPlayer() {
     fetchNBAPlayer();
 }
 
-function getNBAPlayerNews() {
-    const fetchNBAPlayerNews = async () => {
+function getNBAPlayerTeamName(teamID) {
+    
+    const fetchNBAPlayerTeamName = async () => {
         try {
-            const nbaPlayerNewsRes = await fetch("https://newsapi.org/v2/everything?q=james-harden", {
+            const nbaPlayerTeamRes = await fetch(`https://api-nba-v1.p.rapidapi.com/teams/teamId/${teamID}`, {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+                    "x-rapidapi-key": "cfc420df64msha7a3397e1f4de3ep182cedjsnca0bd8135c93"
+                }
+            })
+            const nbaPlayerTeamData = await nbaPlayerTeamRes.json();
+            debugger;
+            const nbaPlayerTeam = nbaPlayerTeamData.api.teams;
+
+            if(nbaPlayerTeam.length === 0){
+                // provide input value once developed
+                throw new Error(`Error pulling team data`);
+            } else {
+                return(nbaPlayerTeam[0].fullName);
+            }
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
+    fetchNBAPlayerTeamName();
+}
+
+function getNBATeam(team) {
+    
+    const fetchNBATeam = async () => {
+        try {
+            const nbaTeamRes = await fetch(`https://api-nba-v1.p.rapidapi.com/teams/nickName/${team}`, {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+                    "x-rapidapi-key": "cfc420df64msha7a3397e1f4de3ep182cedjsnca0bd8135c93"
+                }
+            })
+            const nbaTeamData = await nbaTeamRes.json();
+
+            if(nbaTeamData.api.teams.length === 0){
+                // provide input value once developed
+                throw new Error(`There are no teams in the NBA with the name "${team}"; please try again`);
+            } else {
+                console.log(nbaTeamData);
+            }
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
+    fetchNBATeam();
+}
+
+function getNBAConference(conference) {
+    
+    const fetchNBAConference = async () => {
+        try {
+            const nbaConferenceRes = await fetch(`https://api-nba-v1.p.rapidapi.com/teams/confName/${conference}`, {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+                    "x-rapidapi-key": "cfc420df64msha7a3397e1f4de3ep182cedjsnca0bd8135c93"
+                }
+            })
+            const nbaConferenceData = await nbaConferenceRes.json();
+
+            if(nbaConferenceData.api.teams.length === 0){
+                // provide input value once developed
+                throw new Error(`There is no conferencein the NBA by the name "${conference}"; please try again`);
+            } else {
+                console.log(nbaConferenceData);
+            }
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
+    fetchNBAConference();
+}
+
+function getNBANews(nbaItem) {
+    const fetchNBANews = async () => {
+        try {
+            const nbaNewsRes = await fetch(`https://newsapi.org/v2/everything?q=james-harden`, {
                 "method": "GET",
                 "headers": {
                     "X-Api-Key": "bbbae998198647ef8f363a9b624282de"
                 }
             })
-            const nbaPlayerNews = await nbaPlayerNewsRes.json();
+            const nbaNews = await nbaNewsRes.json();
 
-            if(nbaPlayerNews.status === 'error'){
-                throw new Error(nbaPlayerNews.message);
+            if(nbaNews.status === 'error'){
+                throw new Error(nbaNews.message);
             }
             
-            console.log(nbaPlayerNews);
+            console.log(nbaNews);
 
         } catch(e) {
             console.log(e);
         }
     }
 
-    fetchNBAPlayerNews();
+    fetchNBANews();
 }
 
-function getNBAPlayerVideos() {
-    const fetchNBAPlayerVideos = async () => {
+function getNBAVideos(nbaItem) {
+    const fetchNBAVideos = async () => {
         try {
-            const nbaPlayerVideosRes = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=James%20Harden&safeSearch=strict&key=${youtubeAPIKey}`)
+            const nbaVideosRes = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=James%20Harden&safeSearch=strict&key=${youtubeAPIKey}`)
 
-            const nbaPlayerVideos = await nbaPlayerVideosRes.json();
+            const nbaVideos = await nbaVideosRes.json();
 
-            if(!nbaPlayerVideosRes.ok){
-                throw new Error(nbaPlayerVideos.error.message);
+            if(!nbaVideosRes.ok){
+                throw new Error(nbaVideos.error.message);
             }
             
-            console.log(nbaPlayerVideos);
+            console.log(nbaVideos);
 
         } catch(e) {
             console.log(e);
         }
     }
 
-    fetchNBAPlayerVideos();
+    fetchNBAVideos();
 }
 
-function getNBAPlayerSocial() {
-    const fetchNBAPlayerSocial = async () => {
+function getNBASocialFB(nbaItem) {
+    const fetchNBASocialFB = async () => {
         try {
-            const nbaPlayerSocialRes = await fetch("https://api.social-searcher.com/v2/search?q=james%20harden&network=facebook&key=626e71bd528d38b317758d064c6441c7", {
+            const nbaSocialFBRes = await fetch(`https://api.social-searcher.com/v2/search?q=james%20harden&network=facebook&limit=5&lang=en&key=626e71bd528d38b317758d064c6441c7`, {
                 "method": "GET",
                 "headers": {
                     'Accept':'application/json'
                 }
             })
-            const nbaPlayerSocial = await nbaPlayerSocialRes.json();
+            const nbaSocialFB = await nbaSocialFBRes.json();
 
-            if(!nbaPlayerSocialRes.ok){
-                throw new Error(nbaPlayerSocial.meta.message);
+            if(!nbaSocialFBRes.ok){
+                throw new Error(nbaSocialFB.meta.message);
             }
             
-            console.log(nbaPlayerSocial);
+            console.log(nbaSocialFB);
 
         } catch(e) {
             console.log(e);
         }
     }
 
-    fetchNBAPlayerSocial();
+    fetchNBASocialFB();
 }
 
-function watchForms() {
+function getNBASocialTW(nbaItem) {
+    const fetchNBASocialTW = async () => {
+        try {
+            const nbaSocialTWRes = await fetch(`https://api.social-searcher.com/v2/search?q=james%20harden&network=twitter&limit=5&lang=en&key=626e71bd528d38b317758d064c6441c7`, {
+                "method": "GET",
+                "headers": {
+                    'Accept':'application/json'
+                }
+            })
+            const nbaSocialTW = await nbaSocialTWRes.json();
+
+            if(!nbaSocialTWRes.ok){
+                throw new Error(nbaSocialTW.meta.message);
+            }
+            
+            console.log(nbaSocialTW);
+
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
+    fetchNBASocialTW();
+}
+
+function getNBASocialInsta(nbaItem) {
+    const fetchNBASocialInsta = async () => {
+        try {
+            const nbaSocialInstaRes = await fetch(`https://api.social-searcher.com/v2/search?q=james%20harden&network=instagram&limit=5&lang=en&key=626e71bd528d38b317758d064c6441c7`, {
+                "method": "GET",
+                "headers": {
+                    'Accept':'application/json'
+                }
+            })
+            const nbaSocialInsta = await nbaSocialInstaRes.json();
+
+            if(!nbaSocialInstaRes.ok){
+                throw new Error(nbaSocialInsta.meta.message);
+            }
+            
+            console.log(nbaSocialInsta);
+
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
+    fetchNBASocialInsta();
+}
+
+function getNBASocial(nbaSearchItem) {
+    getNBASocialFB(nbaSearchItem);
+    getNBASocialTW(nbaSearchItem);
+    getNBASocialInsta(nbaSearchItem);
+}
+
+function searchTypeController() {
+    $('#teamButton').click(function() {
+        $(this).addClass('hidden');
+        $('#playerButton').removeClass('hidden');
+        $('#conferenceButton').removeClass('hidden');
+
+        $('#nbaTeamSearch').removeClass('hidden');
+        $('#nbaPlayerSearch').addClass('hidden');
+        $('#nbaConferenceSearch').addClass('hidden');
+    })
+
+    $('#conferenceButton').click(function() {
+        $(this).addClass('hidden');
+        $('#playerButton').removeClass('hidden');
+        $('#teamButton').removeClass('hidden');
+
+        $('#nbaConferenceSearch').removeClass('hidden');
+        $('#nbaPlayerSearch').addClass('hidden');
+        $('#nbaTeamSearch').addClass('hidden');
+    })
+
+    $('#playerButton').click(function() {
+        $(this).addClass('hidden');
+        $('#teamButton').removeClass('hidden');
+        $('#conferenceButton').removeClass('hidden');
+
+        $('#nbaPlayerSearch').removeClass('hidden');
+        $('#nbaConferenceSearch').addClass('hidden');
+        $('#nbaTeamSearch').addClass('hidden');
+    })
+}
+
+function watchPlayerForm() {
     $('#nbaPlayerSearch').submit(event => {
         event.preventDefault();
 
-        getNBAPlayer();
-    })
+        const playerLastName = $('#playerLastName').val();
 
-    $('#nbaPlayerNewsSearch').submit(event => {
-        event.preventDefault();
-
-        getNBAPlayerNews();
-    })
-    
-    $('#nbaPlayerVideoSearch').submit(event => {
-        event.preventDefault();
-
-        getNBAPlayerVideos();
-    })
-
-    $('#nbaPlayerSocialSearch').submit(event => {
-        event.preventDefault();
-
-        getNBAPlayerSocial();
+        getNBAPlayer(playerLastName);
+        getNBANews();
+        getNBAVideos();
+        getNBASocial();
     })
 }
 
-$(watchForms);
+function listeners() {
+    watchPlayerForm();
+    searchTypeController();
+}
+
+$(listeners);
